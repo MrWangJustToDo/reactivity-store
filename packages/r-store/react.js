d2f0405ce51aa4a6a691fe18bc4b81d809cf19ca
MrWangJustToDo <2711470541@qq.com>
2023-09-02 14:43:01 +0800

    revert package structure

--- packages/r-store/react.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./dist/cjs/react.production");
-} else {
-  module.exports = require("./dist/cjs/react.development");
-}
--- packages/r-store/src/index.ts
+++ packages/r-store/src/index.ts
@@ -13,97 +13,41 @@ import {
   onUpdated as _onUpdated,
   onUnmounted as _onUnmounted,
 } from "./store/lifeCycle";
-/**
- * @deprecated
- * try to import from 'reactivity-store/react'
- */
+
 export const createStore = _createStore;
-/**
- * @deprecated
- * try to import from 'reactivity-store/react'
- */
+
 export const createStoreWithComponent = _createStoreWithComponent;
-/**
- * @deprecated
- * try to import from 'reactivity-store/react'
- */
+
 export const onBeforeMount = _onBeforeMount;
-/**
- * @deprecated
- * try to import from 'reactivity-store/react'
- */
+
 export const onBeforeUpdate = _onBeforeUpdate;
-/**
- * @deprecated
- * try to import from 'reactivity-store/react'
- */
+
 export const onBeforeUnmount = _onBeforeUnmount;
-/**
- * @deprecated
- * try to import from 'reactivity-store/react'
- */
+
 export const onMounted = _onMounted;
-/**
- * @deprecated
- * try to import from 'reactivity-store/react'
- */
+
 export const onUpdated = _onUpdated;
-/**
- * @deprecated
- * try to import from 'reactivity-store/react'
- */
+
 export const onUnmounted = _onUnmounted;
-/**
- * @deprecated
- * try to import from 'reactivity-store/react'
- */
+
 export const useReactiveState = _useReactiveState;
-/**
- * @deprecated
- * try to import from 'reactivity-store/react'
- */
+
 export const useReactiveEffect = _useReactiveEffect;
 
-/**
- * @deprecated
- * try to import from 'reactivity-store/react'
- */
 export const createState = _createState;
-/**
- * @deprecated
- * try to import from 'reactivity-store/react'
- */
+
 export const withActions = _withActions;
-/**
- * @deprecated
- * try to import from 'reactivity-store/react'
- */
+
 export const withPersist = _withPersist;
-/**
- * @deprecated
- * try to import from 'reactivity-store/react'
- */
+
 export const withNamespace = _withNameSpace;
 
-/**
- * @deprecated
- * try to import from 'reactivity-store/react'
- */
 export const wrapperBatchUpdate = _wrapperBatchUpdate;
-/**
- * @deprecated
- * try to import from 'reactivity-store/react'
- */
+
 export const getBatch = _getBatch;
-/**
- * @deprecated
- * try to import from 'reactivity-store/react'
- */
+
 export const setBatch = _setBatch;
-/**
- * @deprecated
- * try to import from 'reactivity-store/react'
- */
+
 export const resetBatch = _resetBatch;
 
 export * from "@vue/reactivity";
